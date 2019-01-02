4754b13edd24c44691595640a8c93371e7b49509
Jovan Nikolic <jovannikolic2801@gmail.com>
2019-01-02 11:27:01 +0100

    Credits, buttons

---
+++ Assets/QuitGameButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitGameButton : MonoBehaviour {
+
+	void Start () {
+        gameObject.GetComponent<Button>().
+            onClick.AddListener(ButtonPressed);
+		
+	}
+
+	private void OnMouseUp()
+	{
+
+	}
+
+	private void ButtonPressed()
+	{
+    Application.Quit();
+	Debug.Log("ugaseno");
+	}
+
+}
---
+++ Assets/StartGameButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+
+    public string levelName = "CharacterSelect1";
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Button>().
+            onClick.AddListener(ButtonPressed);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseUp()
+    {
+     
+    }
+
+    private void ButtonPressed()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+}
