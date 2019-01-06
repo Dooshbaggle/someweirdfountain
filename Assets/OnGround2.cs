4968f5c91665377d55d415a72de1d05246764aeb
Dooshbaggle <46221064+Dooshbaggle@users.noreply.github.com>
2019-01-06 21:32:10 +0400

    FCanims2

---
+++ Assets/OnGround2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGround2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponentInParent<CharMovement2>().onGround = true;
+        gameObject.GetComponentInParent<CharMovement2>().jumpNumber =
+            gameObject.GetComponentInParent<CharMovement2>().maxJumpNumber;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.GetComponentInParent<CharMovement2>().onGround = false;
+    }
+}
